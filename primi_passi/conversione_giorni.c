b8b8ed8774bb453cb2d70ab78b770b0aa77c14f0
Michele Gabrieli <72445614+michele25-10@users.noreply.github.com>
2023-10-03 21:01:15 +0200

    Update conversione_giorni.c

--- primi_passi/conversione_giorni.c
+++ primi_passi/conversione_giorni.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-    const int settimane_annue = 52;
-    const int mesi_annui = 12;
+    const int giorni_settimane = 7;
+    const int giorni_mesi = 30;
     const int giorni_annui = 365;
     
     printf("Inserisci il numero di giorni:\n");
     int numero_giorni;
     scanf("%d", &numero_giorni);
 
-    int numero_settimane = numero_giorni / settimane_annue;
+    int numero_settimane = numero_giorni / giorni_settimane;
     printf("\nIl numero di settimane sono: %d", numero_settimane);
 
-    int numero_mesi = numero_giorni/mesi_annui;
-    printf("\nIl numero di mesi sono: %d", numero_settimane);
+    int numero_mesi = numero_giorni/giorni_mesi;
+    printf("\nIl numero di mesi sono: %d", numero_mesi);
 
     int numero_anni = numero_giorni/giorni_annui;
     printf("\nIl numero di anni sono: %d\n", numero_anni);
