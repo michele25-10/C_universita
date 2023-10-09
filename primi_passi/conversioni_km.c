982c6a4dfbe863cbe8b4764969d152369b90d339
michele25-10 <michele.gabri01@gmail.com>
2023-10-09 21:29:28 +0200

    conversioni_km.c

--- primi_passi/conversioni_km.c
+++ primi_passi/conversioni_km.c
@@ -10,6 +10,6 @@ int main()
     float cm = numero_km * (100000);
     float dm = numero_km * (10000);
     float m = numero_km * (1000);
-    printf("\nConversioni: \n-\t%f mm;\n-\t%f cm;\n-\t%f dm;\n-\t%f m", mm, cm, dm, m);
+    printf("\nConversioni: \n-\t%f mm;\n-\t%f cm;\n-\t%f dm;\n-\t%f m\n", mm, cm, dm, m);
     return (0);
 }
