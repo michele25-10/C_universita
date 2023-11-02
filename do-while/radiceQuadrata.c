b6498d517328bf57e9d768288b11255976d56ac4
Michele Gabrieli <72445614+michele25-10@users.noreply.github.com>
2023-11-02 14:50:44 +0100

    Update radiceQuadrata.c con l'uso delle funzioni media e radq

--- do-while/radiceQuadrata.c
+++ do-while/radiceQuadrata.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+float media(float x, float y)
+{
+    return (x + y) / 2.0;
+}
+
+float radq(float n)
+{
+    float successione = 1;
+    do
+    {
+        successione = media(successione, n / successione);
+    } while (!(successione * successione - n > -1e-5 && successione * successione - n < 1e-5));
+    
+    return successione;
+};
+
 int main()
 {
 
@@ -10,14 +26,7 @@ int main()
         scanf("%f", &n);
     } while (n < 0);
 
-    float successione = 1;
-
-    do
-    {
-        successione = (successione + n / successione) / 2.0;
-    } while (! (successione * successione - n > -1e-5 && successione * successione - n < 1e-5));
-
-    printf("%.2f", successione);
+    printf("%.2f\n", radq(n));
 
     return 0;
 }
