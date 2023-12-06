38d41897d53ecab8b01798fc5a9630a4340ce5c9
michele25-10 <michele.gabri01@gmail.com>
2023-12-06 14:44:00 +0100

    Terminato esercizio di sovrascrittura laddove serviva

--- binary_file/aumento_stipendio.c
+++ binary_file/aumento_stipendio.c
@@ -9,35 +9,27 @@ typedef struct{
 int main(){
     Persona v[50]; 
     FILE *fb;
-    fb = fopen("stipendio.dat", "rwb");
+    fb = fopen("stipendio.dat", "r+");
     if(fb == NULL){
         exit(-1);
     } 
 
-    int count = 0;  
+    int count = 0; 
+    fseek(fb, 0, SEEK_SET);
     while(fread(&v[count], sizeof(Persona), 1, fb) > 0){
         printf("%s, %2.f\n", v[count].nome, v[count].stipendio);
+        if(v[count].stipendio < 1000){
+            v[count].stipendio += ((v[count].stipendio)/100)*10; 
+            fseek(fb, ftell(fb)-sizeof(Persona), SEEK_SET);
+            fwrite(&v[count], sizeof(Persona), 1, fb);
+        }
         count ++;
     }
 
     printf("\n\n\n\n");
-    count = 0; 
-    fseek(fb, 0, SEEK_SET);
-    while(fread(&v[count], sizeof(Persona), 1, fb) > 0){
-        if(v[count].stipendio < 1000){
-            v[count].stipendio += ((v[count].stipendio)/100)*10; 
-            printf("%2.f\n", v[count].stipendio);
-            fseek(fb, ftell(fb)-sizeof(Persona), SEEK_SET);
-            fwrite(&v[count], sizeof(Persona), 1, fb);
-            fseek(fb, ftell(fb) + sizeof(Persona), SEEK_SET);
-        }
-        count ++;
-    }
-    fclose(fb);
 
-    fb = fopen("stipendio.dat", "rwb");
     count = 0;  
-    fseek(fb, 0, SEEK_SET);
+    rewind(fb);
     while(fread(&v[count], sizeof(Persona), 1, fb) > 0){
         printf("%s, %2.f\n", v[count].nome, v[count].stipendio);
         count ++;
